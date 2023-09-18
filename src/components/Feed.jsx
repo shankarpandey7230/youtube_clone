aea23d2d1262580810c7c842a138024d008fec9d
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-18 12:55:57 +1000

    Left cateogry section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React, { useContext, useEffect } from "react";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import VideoCard from "./VideoCard";
 const Feed = () => {
-  return <div>Feed</div>;
+  const { loading, searchResults } = useContext(Context);
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow-w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          {!loading &&
+            searchResults.map((item) => {
+              return (
+                <VideoCard key={item?.video?.videoId} video={item?.video} />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
